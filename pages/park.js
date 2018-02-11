bb4125966e816588baf85bd0de7586735c4870dc
Josh Kadis <josh@alleyinteractive.com>
2018-02-10 22:06:54 -0500

    first Next.js route

---
+++ pages/park.js
@@ -0,0 +1,4 @@
+export default (props) => {
+  debugger;
+  return <h1>Hello world</h1>;
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
+const next = require('next');
 
 // Route handlers
 const onAuthCallback = require('./server/onAuthCallback');
@@ -11,34 +12,59 @@ const showRiderData = require('./server/showRiderData');
 const validateApiRequest = require('./api/validateApiRequest');
 const getRanking = require('./api/getRanking');
 
-/* Express Setup */
-const app = express();
+/* Next.js Setup */
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const handle = app.getRequestHandler();
 
-app.get('/', renderApp);
-app.get('/auth-callback', onAuthCallback);
-app.get('/by/:id', showRiderData);
+app.prepare()
+  .then(() => {
 
-/**
- * API routing
- */
-app.get('/api/ranking/:type/:segmentId', async (req, res) => {
-  const validation = validateApiRequest(req.hostname, req.query.key || null);
-  if (validation.error) {
-    res.status(403).json(error);
-    return;
-  }
+    const server = express();
 
-  const responseData = await getRanking(
-    req.params.type,
-    parseInt(req.params.segmentId, 10),
-    req.query.filter
-  );
+    /**
+     * Basic routing
+     */
+    server.get('/', renderApp);
+    server.get('/auth-callback', onAuthCallback);
+    server.get('/by/:id', showRiderData);
 
-  const status = responseData.error ? 500 : 200;
-  res.status(status).json(responseData.error ? responseData : responseData.data);
-});
+    /**
+     * Next.js routing
+     */
+    server.get('/prospectpark', (req, res) => {
+      const { lapSegmentId } = require('./config');
+      app.render(req, res, '/park', { segment: lapSegmentId });
+    });
 
-console.log('Connecting to database');
-mongoose.connect(process.env.MONGODB_URI);
-app.listen(process.env.PORT,
-  () => console.log(`App listening on port ${process.env.PORT}`));
+    /**
+     * API routing
+     */
+    server.get('/api/ranking/:type/:segmentId', async (req, res) => {
+      const validation = validateApiRequest(req.hostname, req.query.key || null);
+      if (validation.error) {
+        res.status(403).json(error);
+        return;
+      }
+
+      const responseData = await getRanking(
+        req.params.type,
+        parseInt(req.params.segmentId, 10),
+        req.query.filter
+      );
+
+      const status = responseData.error ? 500 : 200;
+      res.status(status).json(responseData.error ? responseData : responseData.data);
+    });
+
+    /**
+     * Connect to database and start listening
+     */
+    console.log('Connecting to database');
+    mongoose.connect(process.env.MONGODB_URI);
+    server.listen(process.env.PORT,
+      () => console.log(`App listening on port ${process.env.PORT}`));
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
