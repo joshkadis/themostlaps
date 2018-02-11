0515fe52a81bb72a371cb3a959c92f748f61a1f8
Josh Kadis <josh@alleyinteractive.com>
2018-02-11 16:56:51 -0500

    should work except with FOUC

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
 import * as styles from './Layout.css';
@@ -7,6 +8,9 @@ import * as styles from './Layout.css';
  */
 export default ({ children }) => (
   <div>
+    <Head>
+      <link rel="stylesheet" href="/_next/static/styles.css" />
+    </Head>
     <Header />
     <div className={styles.main}>
       {children}
