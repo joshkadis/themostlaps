150c737d70e193d6cc9e0704ce9c2f0b9a1ffc13
Josh Kadis <josh@alleyinteractive.com>
2018-02-12 10:58:23 -0500

    nav styling

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,10 +1,24 @@
 import Link from 'next/link';
+import classNames from 'classnames';
 import { lapSegmentId } from '../config';
+import * as styles from './Navigation.css';
 
 export default () => (
-  <nav>
-    <Link as="/prospectpark" href={`/park?segment=${lapSegmentId}`}>
-      <a>Prospect Park</a>
-    </Link>
+  <div>
+    <nav>
+      <Link as="/prospectpark" href={`/park?segment=${lapSegmentId}`}>
+        <a className={styles.link}>Prospect Park</a>
+      </Link>
+      <Link href="/about">
+        <a className={styles.link}>About</a>
+      </Link>
+      <Link href="/help">
+        <a className={styles.link}>Help</a>
+      </Link>
+      <span className={classNames(styles.link, styles.ctaLink)}>
+        Post Your Laps
+      </span>
+    }
   </nav>
+  </div>
 );
