d83fe33a0f7eb66fa6e01c36bb89b1954572a5a2
Josh Kadis <josh@alleyinteractive.com>
2018-02-22 09:49:26 -0500

    slim down footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,11 @@
 import * as styles from './Footer.css';
-import { KadiscoSvg } from './lib/svg';
 
 export default () => (
   <footer className={styles.footer}>
-    <KadiscoSvg className={styles.kadiscoSvg}/>
     <span>&copy; 2018 <a href="https://kadisco.com">Josh Kadis</a></span>
     <img
       className={styles.stravaCredit}
-      src="/static/img/api_logo_pwrdBy_strava_stack_white.png"
+      src="/static/img/api_logo_pwrdBy_strava_horiz_white.png"
     />
   </footer>
 );
