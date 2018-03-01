1fa4a162383e105f4f73179481a35215baf3d9cf
Josh Kadis <josh@alleyinteractive.com>
2018-02-28 21:48:02 -0500

    kinda working

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,63 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import Layout from '../components/Layout';
 import Button from '../components/lib/Button';
 import { getPathnameFromContext, APIRequest } from '../utils';
 import { triggerModalOpen } from '../utils/modal';
+import * as styles from '../components/Layout.css';
+
+const delay = {
+  startup: 500,
+  transition: 1000,
+  one: 0,
+  two: 1000,
+  three: 3500,
+  interstitial: 3000,
+};
+
+function unhideChildElements(id) {
+  if (typeof document === 'undefined') {
+    return;
+  }
+  document
+    .querySelectorAll(`#${id} .home__hide`)
+    .forEach((el) => el.classList.remove('home__hide'));
+}
+
+function playAnimation() {
+  if (typeof document === 'undefined') {
+    return;
+  }
+
+  // Unhide first set of elements
+  setTimeout(() => {
+    unhideChildElements('home-primary');
+  }, delay.startup);
+
+  // Unhide second set of elements
+  const secondaryDelay = delay.startup + delay.three + delay.transition + delay.interstitial;
+  setTimeout(() => {
+    document.getElementById('home-primary')
+      .classList.add('home__display-none');
+
+    document.getElementById('home-secondary')
+      .classList.remove('home__display-none');
+
+    setTimeout(() => unhideChildElements('home-secondary'), 0);
+  }, secondaryDelay)
+
+}
 
 class Index extends Component {
   componentDidMount() {
     console.log('MOUNTED');
+    playAnimation();
   }
 
   componentDidUpdate() {
     console.log('UPDATED');
+    playAnimation();
   }
   render() {
     const { pathname, query, siteTotals } = this.props;
@@ -21,17 +67,68 @@ class Index extends Component {
         query={query}
         style={{ textAlign: 'center' }}
       >
-        <Button
-          onClick={triggerModalOpen}
-          style={{
-            marginTop: '1rem',
-            fontSize: '1.25rem',
-            padding: '0.7rem',
-            letterSpacing: '1px',
-          }}
+        <div id="home-primary">
+          <span className={classNames(
+            'home__hide',
+            styles['home__block--big'],
+            styles.home__one
+          )}>
+            Who has the KOM
+          </span>
+          <span className={classNames(
+            'home__hide',
+            styles['home__block--big'],
+            styles.home__two
+          )}>
+            for Prospect Park?
+          </span>
+          <span className={classNames(
+            'home__hide',
+            'bigger',
+            styles.home__block,
+            styles.home__three
+          )}>
+            Who cares.
+          </span>
+        </div>
+
+        <div
+          id="home-secondary"
+          className="home__display-none"
         >
-          Add Your Laps
-        </Button>
+          <span className={classNames(
+            'home__hide',
+            styles['home__block--big'],
+            styles.home__one
+          )}>
+            How many laps
+          </span>
+          <span className={classNames(
+            'home__hide',
+            styles['home__block--big'],
+            styles.home__two
+          )}>
+            have you ridden?
+          </span>
+          <span
+            className={classNames(
+              'home__hide',
+              styles.home__block,
+              styles.home__three
+            )}
+          >
+            <Button
+              onClick={triggerModalOpen}
+              style={{
+                fontSize: '2rem',
+                padding: '1.4rem',
+                letterSpacing: '1px',
+              }}
+            >
+              Find Out Now
+            </Button>
+          </span>
+        </div>
       </Layout>
     );
   }
