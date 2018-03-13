742f153be720542750936f1e2ac4c9ae990c68e1
Josh Kadis <josh@alleyinteractive.com>
2018-03-13 10:59:09 -0400

    simplify mobile nav show/hide

--- components/Navigation.js
+++ components/Navigation.js
@@ -10,61 +10,41 @@ import SocialLink from './lib/SocialLink';
 import { modalControlsShape } from '../utils/propTypes';
 import { trackModalOpen, setDimensions } from '../utils/analytics';
 
-/**
- * Determine if nav should be shown, isomorphically
- */
-function shouldShowNav() {
-  // Show by default from server
-  if ('undefined' === typeof window || !window.innerWidth) {
-    return true;
-  }
-
-  // Hide for small viewport
-  return window.innerWidth >= breakpointPx;
-}
-
 class Navigation extends Component {
   constructor(props) {
     super(props);
-    this.toggleNavLinksContainer = this.toggleNavLinksContainer.bind(this);
+    this.mobileToggleNav = this.mobileToggleNav.bind(this);
     this.onClickButton = this.onClickButton.bind(this);
-  }
-
-  toggleNavLinksContainer(evt) {
-    if (!this.linksContainer || shouldShowNav()) {
-      return;
-    }
-
-    this.linksContainer.classList.toggle(styles.hidden);
+    this.state = {
+      shouldShowMobileNav: false,
+    };
   }
 
   onClickButton() {
+    this.setState({ shouldShowMobileNav: false });
     this.props.modalControls.open();
     setDimensions({ 'Signup Starting Point': 'nav' });
     trackModalOpen();
   }
 
-  componentDidMount() {
-    if (shouldShowNav()) {
-      this.linksContainer.classList.remove(styles.hidden);
-    } else {
-      this.linksContainer.classList.add(styles.hidden);
-    }
+  mobileToggleNav() {
+    this.setState({
+      shouldShowMobileNav: !this.state.shouldShowMobileNav,
+    });
   }
 
   render() {
     return (<div>
       <MenuSvg
         className={styles.menu}
-        onClickHandler={this.toggleNavLinksContainer}
+        onClickHandler={this.mobileToggleNav}
       />
       <nav
         className={classNames(
           styles.linksContainer,
-          { [styles.hidden]: shouldShowNav() || this.props.modalIsOpen }
+          { [styles.hideMobile]: !this.state.shouldShowMobileNav }
         )}
         ref={(el) => { this.linksContainer = el; }}
-        onClick={this.toggleNavLinksContainer}
       >
         <Link href="/ranking">
           <a className={styles.link}>Rankings</a>
@@ -94,6 +74,7 @@ class Navigation extends Component {
 
 Navigation.propTypes = {
   modalControls: PropTypes.shape(modalControlsShape).isRequired,
-}
+  modalIsOpen: PropTypes.bool.isRequired,
+};
 
 export default Navigation;
