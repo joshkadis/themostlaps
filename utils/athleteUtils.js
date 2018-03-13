d52b5cb4b80e327c1e3e84b819b782312a3364c3
Josh Kadis <josh@alleyinteractive.com>
2018-03-12 22:21:40 -0400

    cleaner storage for athlete data model

--- utils/athleteUtils.js
+++ utils/athleteUtils.js
@@ -1,7 +1,7 @@
 const Athlete = require('../schema/Athlete');
 
 /**
- * Convert API response for ahlete to our model's format
+ * Convert API response for athlete to our model's format
  *
  * @param {Object} athlete
  * @param {String} athlete.access_token
@@ -9,15 +9,23 @@ const Athlete = require('../schema/Athlete');
  * @param {Object} athlete.athlete
  * @return {Object}
  */
-function getAthleteModelFormat(athlete) {
+function getAthleteModelFormat({ athlete, access_token, token_type }) {
+  const { firstname, lastname, profile, email, id } = athlete;
   const currentDate = new Date();
   return {
-    _id: athlete.athlete.id,
+    _id: id,
     status: 'ingesting',
     last_updated: currentDate.toISOString(),
     created: currentDate.toISOString(),
     stats: {},
-    ...athlete,
+    access_token,
+    token_type,
+    athlete: {
+      firstname,
+      lastname,
+      profile,
+      email,
+    },
   };
 }
 
