4885675a2388debecec626b9f6a61ea97162912b
Josh Kadis <josh@alleyinteractive.com>
2018-03-28 06:27:18 -0400

    deauthorize Strava API when deleting athlete via CLI

--- cli/deleteUser.js
+++ cli/deleteUser.js
@@ -1,20 +1,42 @@
+const fetch = require('isomorphic-unfetch');
 const Athlete = require('../schema/Athlete');
 const Activity = require('../schema/Activity');
 
 /**
  * Delete user and their activities from the database
+ * Then deauthorize Strava API
  * Then exit the process
  *
  * @param {Number} id
  */
-module.exports = (id) => {
-  Athlete.findByIdAndRemove(id, (err) => {
-    if (err) throw err;
-    console.log(`Deleted user ${id} from athletes collection`);
-    Activity.deleteMany({ athlete_id: id }, (err) => {
-      if (err) throw err;
-      console.log(`Deleted user ${id}'s activities`);
+module.exports = async (id) => {
+  try {
+    // Remove from athletes collection
+    const athleteDoc = await Athlete.findByIdAndRemove(id);
+    if (!athleteDoc) {
+      console.log(`Could not find athlete ID ${id}`);
       process.exit(0);
-    })
-  });
+    } else {
+      console.log(`Deleted user ${id} from athletes collection`);
+    }
+
+    // Remove from activities collection
+    await Activity.deleteMany({ athlete_id: id });
+    console.log(`Deleted user ${id}'s activities`);
+
+    // Deauthorize API access
+    const { status } = await fetch('https://www.strava.com/oauth/deauthorize', {
+      method: 'POST',
+      body: `access_token=${athleteDoc.get('access_token')}`,
+    });
+
+    if (200 === status) {
+      console.log('Deauthorized Strava API');
+    } else {
+      console.log('Error deauthorizing Strava API');
+    }
+    process.exit(0);
+  } catch (err) {
+    throw err;
+  }
 };
