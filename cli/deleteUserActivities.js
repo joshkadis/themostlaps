fb0fd43c7c6dca3c421886717b7daecb0a303785
Josh Kadis <josh@alleyinteractive.com>
2018-04-10 11:45:15 -0400

    reset last_refreshed with deleteactivities cli

--- cli/deleteUserActivities.js
+++ cli/deleteUserActivities.js
@@ -31,6 +31,17 @@ module.exports = async (user, after) => {
     }
   }
 
+  // Reset last_refreshed timestamp
+  const athlete = await Athlete.findById(user);
+  try {
+    athlete.set('last_refreshed', after);
+    const lastUpdatedDoc = await athlete.save();
+    const theDate = new Date(lastUpdatedDoc.get('last_refreshed') * 1000);
+    console.log(`Set last_refreshed to ${theDate.toISOString()}`)
+  } catch (err) {
+    console.log(`Error saving last_refreshed to ${after}`);
+  }
+
   if (savedActivities.length === userActivities.length) {
     console.log('No activities were deleted. Try a longer `daysago` value?');
     process.exit(0);
@@ -38,7 +49,6 @@ module.exports = async (user, after) => {
 
   // Recalculate stats and update the user
   const stats = compileStatsForActivities(savedActivities);
-  const athlete = await Athlete.findById(user);
   await updateAthleteStats(athlete, stats);
   console.log(`Updated stats for ${savedActivities.length} activities. New total is ${stats.allTime}`);
   process.exit(0);
