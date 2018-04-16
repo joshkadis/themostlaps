8c31e3d6a0f121b8b0fedce60f3cae5897c31ed9
Josh Kadis <josh@alleyinteractive.com>
2018-04-16 14:37:37 -0400

    error modal update

--- components/modal/AuthError.js
+++ components/modal/AuthError.js
@@ -7,8 +7,9 @@ const AuthError = ({ code, id }) => (
   <div>
     <p>{getInternalError(code, id)}</p>
     {50 === code && id !== 0 &&
-      <p>Visit your <Link href={`/rider?athleteId=${id}`} as={`/rider/${id}`}><a>rider page</a></Link>.</p>
+      <p>We check for new rides automatically to update your stats. You can visit your <Link href={`/rider?athleteId=${id}`} as={`/rider/${id}`}><a>rider page</a></Link> any time without reconnecting to Strava.</p>
     }
+    <p>Something not working? Email <a href="mailto:info@themostlaps.com">info@themostlaps.com</a> and let us know what's up.</p>
   </div>
 );
 
