d2c21c22c1974dd328732482b2aabaf31c7785ce
Josh Kadis <josh@alleyinteractive.com>
2018-04-19 09:59:05 -0400

    delete auth-callback handler

--- server.js
+++ server.js
@@ -33,49 +33,6 @@ app.prepare()
     server.get('/auth-callback', async (req, res) => {
       handleSignupCallback(req, res);
     });
-    // server.get('/auth-callback', async (req, res) => {
-    //   const authResult = await onAuthCallback(req, res);
-
-    //   let redirectQuery;
-    //   if (authResult.error || !authResult.athlete) {
-
-    //     // Pass athlete ID if available
-    //     const id = authResult.athlete && authResult.athlete.id ?
-    //       authResult.athlete.id :
-    //       0;
-
-    //     redirectQuery = {
-    //       autherror: authResult.error || 99,
-    //       id,
-    //     };
-    //     console.log(authResult);
-    //     const errorInfo = authResult.athlete && authResult.athlete.id ?
-    //       `Athlete id ${authResult.athlete.id}, ${authResult.athlete.email || 'email unknown'}` :
-    //       false;
-
-    //     slackError(authResult.error, errorInfo);
-    //   } else {
-    //     redirectQuery = {
-    //       authsuccess: 'true',
-    //       firstname: authResult.athlete.firstname,
-    //       allTime: authResult.athlete.allTime,
-    //       id: authResult.athlete.id,
-    //     };
-    //   }
-
-    //   const stateParam = req.query.state ?
-    //     decodeURIComponent(req.query.state).split('|') :
-    //     ['/'];
-
-    //   if (redirectQuery.authsuccess) {
-    //     subscribeToMailingList(
-    //       authResult.athlete.email,
-    //       (stateParam.length > 1 && stateParam[1] === 'shouldSubscribe') // Add to newsletter segment?
-    //     );
-    //   }
-
-    //   res.redirect(303, `${stateParam[0]}?${querystring.stringify(redirectQuery)}`);
-    // });
 
     /**
      * Next.js routing
