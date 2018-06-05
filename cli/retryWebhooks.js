ce31633f92197f6c7e30805bd2d4d00bd3812006
Josh Kadis <josh@alleyinteractive.com>
2018-06-04 23:05:32 -0400

    select and iterate over activities to retry

--- cli/retryWebhooks.js
+++ cli/retryWebhooks.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
+const refreshAthleteFromActivity =
+  require('../utils/refreshAthlete/refreshAthleteFromActivity');
 
 // YYYY-MM-DD to integer
 const dateStringToInt = (date) => parseInt(date.replace(/-/g, ''), 10);
@@ -12,13 +15,83 @@ const intToDateString = (int) => {
   return digits.join('');
 }
 
+function getActivityDataToRetry({ attachments }) {
+  let shouldRetry = true;
+  try {
+    shouldRetry = attachments[0].pretext === 'Error code 110' &&
+      attachments[0].fields[1].title === 'Details';
+  } catch (error) {
+    return false;
+  }
+  if (!shouldRetry) {
+    return false;
+  }
+
+  try {
+    const activity = JSON.parse(attachments[0].fields[1].value);
+    if (activity.aspect_type !== 'create') {
+      return false;
+    }
+
+    return {
+      owner_id: activity.owner_id,
+      object_id: activity.object_id,
+    };
+  } catch (error) {
+    return false;
+  }
+
+}
+
+function getActivitiesFromFile(filename) {
+  let logData = [];
+  try {
+    logData = JSON.parse(fs.readFileSync(filename, 'utf8'));
+  } catch (error) {
+    console.log(`Error parsing ${filename}`);
+    return null;
+  }
+
+  if (!logData || !logData.length) {
+    console.log(`${filename} contained no data`);
+    return null;
+  }
+
+  return logData.reduce((acc, message) => {
+    const activityData = getActivityDataToRetry(message);
+    return activityData ? [...acc, activityData] : acc;
+  }, []);
+}
+
+
 async function retryWebhooks(startdate, dryrun) {
   const logFilesPath = path.resolve(__dirname, '../slack-logs');
   const logFiles = glob.sync(logFilesPath + '/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9].json')
     .map((filename) => path.basename(filename, '.json'))
     .map(dateStringToInt)
     .filter((logDate) => (!isNaN(logDate) && logDate >= startdate))
-    .map((int) => `${logFilesPath}/${intToDateString(int)}.json`);
+    .map(intToDateString);
+
+  console.log(`Retrying ${logFiles.length} log files`);
+
+  const activitiesToRetry = logFiles.reduce((acc, date) => {
+    const filename = `${logFilesPath}/${date}.json`;
+    const fileActivities = getActivitiesFromFile(filename);
+    console.log(`Found ${fileActivities.length} on ${date}`);
+    return fileActivities
+      ? acc.concat(fileActivities)
+      : acc;
+  }, []);
+
+  for (let idx = 0; idx < activitiesToRetry.length; idx++) {
+    const { owner_id, object_id } = activitiesToRetry[idx];
+    let laps;
+    try {
+      laps = await refreshAthleteFromActivity(owner_id, object_id, !dryrun);
+    } catch (err) {
+      // log ouput provided by refreshAthleteFromActivity
+    }
+  }
 
   process.exit(0);
 }
