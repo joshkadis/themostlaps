32e447642c009938659b02f7e3522dd92b7c6ca9
joshkadis <josh@kadisco.com>
2018-12-11 22:52:39 -0500

    fetch cold laps points in activityinfo cli

--- cli/getActivityInfo.js
+++ cli/getActivityInfo.js
@@ -1,5 +1,7 @@
 const Athlete = require('../schema/Athlete');
+const Activity = require('../schema/Activity');
 const fetchLapsFromActivities = require('../utils/refreshAthlete/fetchLapsFromActivities');
+const { getColdLapsFromActivity } = require('../utils/stats/compileSpecialStats');
 
 async function getActivityInfo(userId, activityId) {
   const athleteDoc = await Athlete.findById(userId, 'access_token');
@@ -13,6 +15,12 @@ async function getActivityInfo(userId, activityId) {
     athleteDoc.get('access_token'),
     true
   );
+
+  const activityDoc = await Activity.findById(activityId);
+  if (activityDoc) {
+    const coldLapsPoints = await getColdLapsFromActivity(activityDoc, true);
+    activityInfo.coldLapsPoints = coldLapsPoints;
+  }
 
   console.log(JSON.stringify(activityInfo[0], null, 4));
   process.exit(0);
