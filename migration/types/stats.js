b86e0a242f0790665ae819d12270cb7af0f19a84
joshkadis <josh@kadisco.com>
2019-01-06 13:05:14 -0500

    update stats migration to handle special -> giro2018 and cold2019

--- migration/types/stats.js
+++ migration/types/stats.js
@@ -4,6 +4,9 @@ const {
 } = require('../gqlQueries');
 const { getAthleteDoc } = require('../helpers');
 
+/**
+  Get gql query for a specific stat
+**/
 const getStatCreateData = (id, key, type, value) => `{
   athlete: {
     connect: {
@@ -15,34 +18,42 @@ const getStatCreateData = (id, key, type, value) => `{
   value: ${value}
 }`;
 
+/**
+  Create array of gql queries for each individual stat *except* years
+  Note: Discarding year totals, will replace with custom resolver
+  that adds up monthly totals
+  @todo: Monthly totals could be replaced by query ActivityLaps where
+  Activity is within a date range then cached somewhere
+**/
 function getStatCreateArgs(user, stats) {
-  return Object.keys(stats).map((key) => {
+  // use reduce instead of map because special contains giro2018 and cold2019
+  return Object.keys(stats).reduce((acc, key) => {
     const stat = stats[key];
-    // giro2018 is the only "special" stat we have to worry about
-    if (key === 'special' && stat.giro2018) {
-      return getStatCreateData(user, 'giro2018', 'special', stat.giro2018);
+    if (key === 'special') {
+      if (stat.giro2018) {
+        acc = [...acc, getStatCreateData(user, 'giro2018', 'special', stat.giro2018)];
+      }
+      if (stat.cold2019) {
+        acc = [...acc, getStatCreateData(user, 'cold2019', 'special', stat.cold2019)];
+      }
     }
 
     // Handle all-time and single-ride totals
     if (key === 'allTime' || key === 'single') {
-      return getStatCreateData(user, key, key, stat);
+      acc = [...acc, getStatCreateData(user, key, key, stat)];
     }
 
-    // Handle month totals
-    // Note: Discarding year totals, will replace with custom resolver
-    // that adds up monthly totals
-    // @todo: Monthly totals could be replaced by query ActivityLaps where
-    // Activity is within a date range then cached somewhere
     const dateParts = key.split('_').filter(part => part.length);
     if (dateParts.length === 2) {
-      return getStatCreateData(user, dateParts.join('_'), 'month', stat);
-    } else {
-      return null;
+      acc = [...acc, getStatCreateData(user, dateParts.join('_'), 'month', stat)];
     }
-  })
-    .filter(el => !!el);
+    return acc;
+  }, []);
 }
 
+/**
+  Migrate all stats for Athlete by idea
+**/
 async function migrateAthleteStats(user, force) {
   // Check that user's athlete data has been migrated
   const gqlAthlete = await getGqlAthlete(user,`{
