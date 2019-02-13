cb93f702350eb3ee49f32d349051817358daf9cc
joshkadis <josh@kadisco.com>
2019-02-13 06:58:23 -0500

    unit test for getDocFromMaybeToken

--- utils/athleteUtils.test.js
+++ utils/athleteUtils.test.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Athlete = require('../schema/Athlete');
 const {
   getEpochSecondsFromDateObj,
+  getDocFromMaybeToken
 } = require('./athleteUtils');
 
 test('getEpochSecondsFromDateObj', () => {
@@ -14,3 +18,44 @@ test('getEpochSecondsFromDateObj', () => {
   expect(getEpochSecondsFromDateObj(dateLocal)).toBe(localTimestamp);
 });
 
+test('getDocFromMaybeToken', async () => {
+  const athlete = new Athlete({ _id: 1, access_token: 'my_token' });
+  expect(athlete instanceof mongoose.Document).toBe(true);
+
+  let expected = await getDocFromMaybeToken(athlete);
+  expect(expected instanceof mongoose.Document).toBe(true);
+  expect(expected.get('access_token')).toBe('my_token');
+
+  expected = await getDocFromMaybeToken(20);
+  expect(expected).toBe(null);
+
+  expected = await getDocFromMaybeToken({ hello: 'there' });
+  expect(expected).toBe(null);
+
+  /**
+    CAUTION!!! opens live connection to database
+  **/
+  try {
+    mongoose.connect(process.env.MONGODB_URI);
+    const db = mongoose.connection;
+    db.on('close', () => {
+      process.exit(0);
+    });
+    db.once('open', async () => {
+      // Save model then find in DB then delete
+      await athlete.save({ validateBeforeSave: false });
+      const found = await getDocFromMaybeToken('my_token');
+      expect(found instanceof mongoose.Document).toBe(true);
+      expect(found.get('_id')).toBe(1);
+      expect(found.get('access_token')).toBe('my_token');
+      await Athlete.deleteOne({ _id: 1 });
+
+      const notFound = await getDocFromMaybeToken('not_a_real_token');
+      expect(notFound).toBe(null);
+
+      db.close();
+    });
+  } catch (err) {
+    console.log('Could not connect to database to test fetching by token');
+  }
+});
