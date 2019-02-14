617d21ce3678a4409f02e3ce4b89e318b911010c
Josh Kadis <997097+joshkadis@users.noreply.github.com>
2019-02-14 11:47:34 -0500

    Handle int32 activity overflow
    https://groups.google.com/forum/#!topic/strava-api/eVCHNjaTOSA

--- utils/refreshAthlete/refreshAthleteFromActivity.js
+++ utils/refreshAthlete/refreshAthleteFromActivity.js
@@ -72,6 +72,12 @@ async function updateAthleteLastRefreshed(athleteDoc, startDateString) {
  * @param {Bool} shouldUpdateDb If false, will validate without saving
  */
 async function refreshAthleteFromActivity(athleteId, activityId, shouldUpdateDb = true) {
+  // Handle int32 overflow
+  // https://groups.google.com/forum/#!topic/strava-api/eVCHNjaTOSA
+  if (activityId < 0) {
+    activityId += 2 * (2147483647 + 1)
+  }
+  
   // Check that athlete exists and activity is new
   let athleteDoc = await Athlete.findById(athleteId);
   if (!athleteDoc) {
