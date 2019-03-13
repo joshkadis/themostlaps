9c4b319dc0a7fc057b1dca4bc23562c484f1167a
joshkadis <josh@kadisco.com>
2019-03-13 10:55:05 -0400

    emoji to indicate success/failure (success so far!)

--- cli/migrateAuthToken.js
+++ cli/migrateAuthToken.js
@@ -8,7 +8,7 @@ async function migrateSingle(athleteId, isDryRun) {
     // Check that Athlete exists
     const athleteDoc = await Athlete.findById(athleteId);
     if (!athleteDoc) {
-      console.log(`Could not locate athlete ${athleteId}`);
+      console.log(`❌ Could not locate athlete ${athleteId}`);
       process.exit(0);
     }
 
@@ -33,7 +33,7 @@ async function migrateSingle(athleteId, isDryRun) {
 
     const receivedAccessToken = await getUpdatedAccessToken(athleteDoc, true);
     if (!receivedAccessToken) {
-      console.log('getUpdatedAccessToken failed')
+      console.log('❌ getUpdatedAccessToken failed')
       process.exit(0)
     }
 
@@ -41,13 +41,13 @@ async function migrateSingle(athleteId, isDryRun) {
 
     const newAthleteDoc = await Athlete.findById(athleteId);
     console.log(receivedAccessToken === newAthleteDoc.get('access_token') ?
-      'Received token matches saved token' :
-      `Saved token ()${newAthleteDoc.get('refresh_token')}) does not match received token`
+      '✅ Received token matches saved token' :
+      `❌ Saved token ()${newAthleteDoc.get('refresh_token')}) does not match received token`
     );
 
     const newExpiresAt = newAthleteDoc.get('expires_at');
     const expirationDate = new Date(newExpiresAt * 1000);
-    console.log(`Refresh token expires at ${expirationDate.toString()}`);
+    console.log(`✅ Refresh token expires at ${expirationDate.toString()}`);
 
     process.exit(0);
 }
