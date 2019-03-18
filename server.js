99801c94f55ea33f111c29939b382b0d98a07343
joshkadis <josh@kadisco.com>
2019-03-18 19:08:05 -0400

    use different config var for staging

--- server.js
+++ server.js
@@ -102,16 +102,15 @@ app.prepare()
     /**
      * Connect to database and start listening
      */
-    console.log(`MONGODB_URI before connection: ${process.env.MONGODB_URI}`);
-    mongoose.connect(process.env.MONGODB_URI);
+    console.log(`Connecting to MONGODB_URI${process.env.MONGODB_URI ? '' : '_STAGING'} : ${process.env.MONGODB_URI || process.env.MONGODB_URI_STAGING}`);
+    mongoose.connect(process.env.MONGODB_URI || process.env.MONGODB_URI_STAGING);
     const db = mongoose.connection;
     db.once('open', () => {
-      console.log(`MONGODB_URI after connection: ${process.env.MONGODB_URI}`);
       console.log('Connected to database');
       server.listen(process.env.PORT, () => {
         console.log(`App listening on port ${process.env.PORT}`);
         if (!process.env.DISABLE_NIGHTLY_REFRESH) {
-          scheduleNightlyRefresh();          
+          scheduleNightlyRefresh();
         }
       });
     });
