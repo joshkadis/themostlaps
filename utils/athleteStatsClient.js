2f43b8d2d3c280c23d60923f83fa264076a99e92
joshkadis <josh@kadisco.com>
2019-11-03 22:14:36 -0500

    new function to parse raw stats object from API

--- utils/athleteStatsClient.js
+++ utils/athleteStatsClient.js
@@ -9,6 +9,63 @@ const DEFAULT_OUTPUT = {
   data: {},
 };
 
+const DEFAULT_OUTPUT_V2 = {
+  allTime: 0,
+  single: 0,
+  byYear: {},
+  byMonth: {},
+  availableYears: [],
+};
+
+function parseRawAthleteStats(rawStats = {}) {
+  // If either of these is undefined or 0, we can exit
+  if (!rawStats.allTime || !rawStats.single) {
+    return DEFAULT_OUTPUT_V2;
+  }
+
+  const parsed = { ...DEFAULT_OUTPUT_V2 };
+  let byYear = [];
+  let byMonth = [];
+  let availableYears = [];
+
+  const keys = Object.keys(rawStats);
+  // Keys will be like _2018, _2018_01
+  // This sorts them by year and months within a year
+  keys.sort();
+  keys.forEach((key) => {
+    const value = rawStats[key];
+    if (key === 'allTime' || key === 'single') {
+      parsed[key] = value;
+      return;
+    }
+
+    const matches = /^_(\d{4,4})_?(\d{2,2})?$/.exec(key);
+    if (!matches) {
+      return;
+    }
+
+    const year = parseInt(matches[1], 10);
+    const month = matches[2]
+      ? parseInt(matches[2], 10)
+      : false;
+
+    if (month === false) {
+      byYear = [...byYear, [year, value]];
+      availableYears = [...availableYears, year];
+    } else {
+      byMonth = [...byMonth, [year, month, value]];
+    }
+  });
+
+  return {
+    ...parsed,
+    availableYears,
+    byMonth,
+    byYear,
+  };
+}
+
+
 /**
  * Format athlete stats for rider page
  *
@@ -187,6 +244,7 @@ module.exports = {
   mergeStatsSingleYear,
   getMinMaxYears,
   getStatsForLocation,
+  parseRawAthleteStats,
   statsForAthletePage,
   statsForSingleAthleteChart,
   statsForSingleAthleteYearChart,
--- utils/athleteStatsClient.test.js
+++ utils/athleteStatsClient.test.js
@@ -1,9 +1,55 @@
 const {
   getMinMaxYears,
   mergeStats,
+  parseRawAthleteStats,
   statsForAthletePage,
   statsForSingleAthleteChart,
 } = require('./athleteStatsClient');
+
+const SAMPLE_RAW_STATS = {
+  allTime: 189,
+  single: 13,
+  _2017: 33,
+  _2017_01: 10,
+  _2017_02: 11,
+  _2017_03: 12,
+  _2016: 93,
+  _2016_01: 30,
+  _2016_02: 31,
+  _2016_03: 32,
+  _2018: 63,
+  _2018_01: 20,
+  _2018_02: 21,
+  _2018_03: 22,
+};
+
+test('parseRawAthleteStats', () => {
+  expect(parseRawAthleteStats(SAMPLE_RAW_STATS)).toEqual({
+    allTime: 189,
+    single: 13,
+    availableYears: [
+      2016,
+      2017,
+      2018,
+    ],
+    byYear: [
+      [2016, 93],
+      [2017, 33],
+      [2018, 63],
+    ],
+    byMonth: [
+      [2016, 1, 30],
+      [2016, 2, 31],
+      [2016, 3, 32],
+      [2017, 1, 10],
+      [2017, 2, 11],
+      [2017, 3, 12],
+      [2018, 1, 20],
+      [2018, 2, 21],
+      [2018, 3, 22],
+    ],
+  });
+});
 
 test('statsForAthletePage', () => {
   expect(statsForAthletePage({
@@ -33,22 +79,7 @@ test('statsForAthletePage', () => {
       },
     });
 
-  expect(statsForAthletePage({
-    allTime: 189,
-    single: 13,
-    _2017: 33,
-    _2017_01: 10,
-    _2017_02: 11,
-    _2017_03: 12,
-    _2016: 93,
-    _2016_01: 30,
-    _2016_02: 31,
-    _2016_03: 32,
-    _2018: 63,
-    _2018_01: 20,
-    _2018_02: 21,
-    _2018_03: 22,
-  }))
+  expect(statsForAthletePage(SAMPLE_RAW_STATS))
     .toEqual({
       allTime: 189,
       single: 13,
