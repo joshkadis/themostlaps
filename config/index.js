b14953ab7aad1956883c3af579d841d2df284837
joshkadis <josh@kadisco.com>
2019-11-05 06:37:45 -0500

    only add to config, no deletions

--- config/index.js
+++ config/index.js
@@ -3,10 +3,26 @@ const prospectpark = require('./locations/prospectpark');
 module.exports = {
   apiUrl: 'https://www.strava.com/api/v3',
   defaultLocation: 'prospectpark',
-  locations: [
+  locations: {
     prospectpark,
-  ],
+  },
   apiPerPage: 30,
+  parkCenter: {
+    latitude: 40.661990,
+    longitude: -73.969681,
+  },
+  allowedRadius: 50000,
+  minDistance: 3300,
+  addMakeupLap: true, // Avoids excess API calls when ingesting new user
+  devFetchActivities: 10,
+  lapSegmentId: 5313629, // Prospect Park Race Lap
+  sectionSegmentIds: [
+    613198, // Prospect Park hill
+    4435603, // Top of Prospect Park
+    4362776, // Prospect Pure Downhill
+    9699985, // Sprint between the lights
+    740668, // E Lake Drive
+  ],
   authRequestParams: {
     client_id: process.env.CLIENT_ID,
     response_type: 'code',
