0113c034c231e46646a04e9d1d6928baed8c4cce
Josh Kadis <josh@kadisco.com>
2019-12-13 23:53:04 -0500

    temp code to refresh everyone

--- utils/scheduleNightlyRefresh.js
+++ utils/scheduleNightlyRefresh.js
@@ -1,28 +1,50 @@
+/* eslint-disable no-await-in-loop, no-underscore-dangle */
 const { scheduleJob } = require('node-schedule');
 const { refreshSchedule } = require('../config');
 const Athlete = require('../schema/Athlete');
 const refreshAthleteActivities = require('./refreshAthlete');
 const { timePartString } = require('./dateTimeUtils');
 const refreshAthleteProfile = require('./refreshAthlete/refreshAthleteProfile');
-
+const { daysAgoTimestamp } = require('../cli/utils');
 /**
  * Query athletes collection then refresh since last_updated
  *
  * @param {Bool} shouldRefreshActivities Default to false, i.e. update profile
  * @param {Multiple} findArgs Args to pass to http://mongoosejs.com/docs/api.html#find_find
  */
-async function refreshAthletes(shouldRefreshActivities = false, findArgs = [{}]) {
+async function refreshAthletes(
+  shouldRefreshActivities = false,
+  findArgs = [{}],
+) {
   const athletes = await Athlete.find(...findArgs);
   for (let i = 0; i < athletes.length; i++) {
     const athlete = athletes[i];
     const updatedAthlete = await refreshAthleteProfile(athlete);
-    if (updatedAthlete && shouldRefreshActivities) {
-      await refreshAthleteActivities(
-        updatedAthlete,
-        false,
-        process.env.SHOULD_REFRESH_VERBOSE
-      );
+    /* TEMP CODE */
+    try {
+      // Be cautious since going straight to production...
+      if (
+        typeof athlete.get('stats._2019') === 'number'
+        && athlete.get('stats._2019') > 0
+      ) {
+        await refreshAthleteActivities(
+          athlete,
+          daysAgoTimestamp(15),
+          true,
+        );
+      }
+    } catch (err) {
+      const identifier = `${athlete.id} | ${athlete.get('athlete.firstname')} ${athlete.get('athlete.lastname')}`;
+      console.log(`refreshAthleteActivities failed | ${identifier}`);
     }
+    /* /TEMP CODE */
+    // if (updatedAthlete && shouldRefreshActivities) {
+    //   await refreshAthleteActivities(
+    //     updatedAthlete,
+    //     false,
+    //     process.env.SHOULD_REFRESH_VERBOSE
+    //   );
+    // }
   }
   console.log(`Updated ${athletes.length} athletes`);
 }
