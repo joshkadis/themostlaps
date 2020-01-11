179a7789a53112794a0a0687d78433e78f07b291
Josh Kadis <josh@kadisco.com>
2020-01-11 10:34:10 -0500

    specify where activity was ruled ineligible

--- utils/v2/activityQueue/ingestActivityFromStravaData.js
+++ utils/v2/activityQueue/ingestActivityFromStravaData.js
@@ -80,7 +80,7 @@ async function ingestActivityFromStravaData(
   if (!activityCouldHaveLaps(rawActivity, true)) {
     return {
       status: 'dequeued',
-      detail: 'activityCouldHaveLaps() returned false',
+      detail: 'activityCouldHaveLaps() returned false during ingest',
     };
   }
 
