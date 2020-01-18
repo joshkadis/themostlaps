45fcdbed7495eee6ab9be146578340a28c7415d4
Josh Kadis <josh@kadisco.com>
2020-01-18 17:18:28 -0500

    add stats_version field to Athlete schema

--- schema/Athlete.js
+++ schema/Athlete.js
@@ -27,6 +27,7 @@ const athleteSchema = new Schema(
       required: true,
       default: ['prospectpark'],
     },
+    stats_version: { type: String, required: true, default: 'v1' },
     preferences: {
       notifications: {
         monthly: { type: Boolean, default: true },
