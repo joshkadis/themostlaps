9633ecddcf5aaa7a6eb7a8f7906a1e1120395821
Josh Kadis <josh@kadisco.com>
2020-01-22 09:47:28 -0500

    fix async iterable

--- utils/v2/asyncUtils.js
+++ utils/v2/asyncUtils.js
@@ -8,10 +8,12 @@
 function makeArrayAsyncIterable(source, iterator) {
   // eslint-disable-next-line no-param-reassign
   source[Symbol.asyncIterator] = () => ({
+    idx: 0,
     // eslint-disable-next-line func-names
     async next() {
-      if (source.length) {
-        const result = await iterator(source.pop());
+      if (this.idx < source.length) {
+        const result = await iterator(source[this.idx]);
+        this.idx += 1;
         return Promise.resolve({ value: result, done: false });
       }
       return Promise.resolve({ done: true });
