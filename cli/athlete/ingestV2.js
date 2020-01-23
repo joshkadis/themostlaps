5b2f91d3ac1ef136d23b1663fb39617f7ffddcac
Josh Kadis <josh@kadisco.com>
2020-01-22 22:18:57 -0500

    remove unnecessary call to clear athlete's history

--- cli/athlete/ingestV2.js
+++ cli/athlete/ingestV2.js
@@ -2,7 +2,6 @@ const { setupConnection } = require('../utils/setupConnection');
 const { makeCheckNumArgs } = require('../utils');
 const Athlete = require('../../schema/Athlete');
 const ingestAthleteHistory = require('../../utils/v2/ingestAthlete/ingestAthleteHistory');
-const { clearAthleteHistoryV2 } = require('../../utils/v2/models/athlete');
 
 const checkNumArgs = makeCheckNumArgs('Use format: $ athlete ingestv2');
 
@@ -27,10 +26,6 @@ async function doCommand({
   if (!athleteDoc) {
     console.warn(`Athlete ${athleteId} not found`);
     return;
-  }
-
-  if (!isDryRun) {
-    await clearAthleteHistoryV2(athleteDoc);
   }
 
   if (isDryRun) {
