1e4124953750eb2492f96c6af72aa5d4844afe26
Josh Kadis <josh@kadisco.com>
2020-01-23 23:59:58 -0500

    unused functions: athleteStats

--- utils/v2/stats/athleteStats.js
+++ utils/v2/stats/athleteStats.js
@@ -1,100 +1,11 @@
 const _cloneDeep = require('lodash/cloneDeep');
-const { defaultLocation } = require('../../../config');
 const { defaultV2Stats } = require('../../../config/stats');
-
-const { getMonthName, getMonthKey } = require('../../../utils/dateTimeUtils');
+const { getMonthKey } = require('../../../utils/dateTimeUtils');
 
 const getDefaultV2Stats = (overrides) => _cloneDeep({
   ...defaultV2Stats,
   ...overrides,
 });
-
-/**
- * Create array of month-value objects to receive monthly stats
- *
- * @return {Array}
- */
-function setUpYearByMonths() {
-  return Array(12)
-    .fill(null)
-    .map((val, idx) => ({
-      month: getMonthName(idx + 1, 3), // Jan, Feb, Mar...
-      value: 0,
-    }));
-}
-
-/**
- * Transform stats from format in database to format for rider page
- *
- * @param {Object} rawStats
- * @param {Object} overrides Optional info applied just before returning
- * @return {Object}
- */
-function transformAthleteStats(rawStats = {}, overrides = {}) {
-  // If either of these is undefined or 0, we can exit
-  if (!rawStats.allTime || !rawStats.single) {
-    return getDefaultV2Stats();
-  }
-
-  // Ready to override default properties
-  const namedStats = getDefaultV2Stats();
-  let byYear = [];
-  const byMonth = {};
-  let availableYears = [];
-
-  const keys = Object.keys(rawStats);
-  // Keys will be like _2018, _2018_01
-  // So this sorts them by year and months within a year
-  keys.sort();
-  keys.forEach((key) => {
-    const value = rawStats[key];
-    if (key === 'allTime' || key === 'single') {
-      namedStats[key] = value;
-      return;
-    }
-
-    // Only match _YYYY and _YYYY_MM
-    const matches = /^_(\d{4})(?:_(\d{2}))?$/.exec(key);
-    if (!matches) {
-      return;
-    }
-
-    const year = parseInt(matches[1], 10);
-    const month = matches[2]
-      ? parseInt(matches[2], 10)
-      : false;
-
-    if (month === false) {
-      byYear = [...byYear, { value, year }];
-      availableYears = [...availableYears, year];
-    } else if (month <= 12) {
-      byMonth[year] = byMonth[year] || setUpYearByMonths();
-      // Set value for 1-based month as 0-based index
-      byMonth[year][(month - 1)].value = value;
-    }
-  });
-
-  return {
-    ...namedStats,
-    availableYears,
-    byMonth,
-    byYear,
-    ...overrides,
-  };
-}
-
-/**
- * Get rider's stats for a location
- *
- * @param {Object} stats
- * @param {String} location
- * @return {Object|Bool} Transformed stats or false if location not found
- */
-function getStatsForLocation(locationsObj, location = defaultLocation) {
-  return locationsObj && locationsObj[location]
-    ? transformAthleteStats(locationsObj[location])
-    : false;
-}
 
 /**
  * Set updated athlete v1 stats object from single activity
@@ -125,6 +36,4 @@ function updateAthleteStatsFromActivity(athleteDoc, delta, startDate) {
 module.exports = {
   getDefaultV2Stats,
   updateAthleteStatsFromActivity,
-  getStatsForLocation,
-  transformAthleteStats,
 };
