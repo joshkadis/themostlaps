137f406fd19854fb7a4acd1049ad84903115a900
Josh Kadis <josh@kadisco.com>
2020-02-15 15:28:12 -0500

    handle locations with nosectionSegmentIds

--- utils/v2/stats/calculateLaps.js
+++ utils/v2/stats/calculateLaps.js
@@ -12,6 +12,12 @@ const { getLocationNameFromSegmentId } = require('../locations');
  */
 function getSegmentSequences(canonicalId) {
   const locName = getLocationNameFromSegmentId(canonicalId);
+  if (!allLocations[locName].sectionSegmentIds
+    || !allLocations[locName].sectionSegmentIds.length
+  ) {
+    return [];
+  }
+
   const sectionIds = [...allLocations[locName].sectionSegmentIds];
   const numSections = sectionIds.length;
   const sequences = [];
