313b09e62cfaa1051bcbd9ecad2994a60544dc3a
Josh Kadis <josh@kadisco.com>
2020-02-21 11:28:54 -0800

    i think that will do it for the /ranking params

--- server/ranking.js
+++ server/ranking.js
@@ -4,9 +4,33 @@ const { getLocationNames } = require('../utils/v2/locations');
 
 const locationsReStr = getLocationNames().join('|');
 const yearReStr = '(?:20[12]\\d)';
-const monthReStr = '(?:0\\d)|1[0-2]';
-const rankingTypeReStr = `${allowedRankingTypes.join('|')}|${yearReStr}`;
 
+/**
+ * Validate reqPrimary and reqSecondary
+ *
+ * @param {String} params.reqPrimary
+ * @param {String} params.reqSecondary
+ * @returns {Bool}
+ */
+function requestParamsAreValid({ reqPrimary = '', reqSecondary = '' }) {
+  const primaryIsValidType = reqPrimary && reqPrimary.match(
+    new RegExp(`(${allowedRankingTypes.join('|')}`),
+  );
+  const primaryIsValidYear = reqPrimary && reqPrimary.match(
+    /(?:20[12]\d)/,
+  );
+  const secondaryIsValidMonth = !reqSecondary || reqSecondary.match(
+    /(?:(?:0\d)|1[0-2])/,
+  );
+
+  if (
+    (primaryIsValidType && !reqSecondary) // /ranking/single
+    || (primaryIsValidYear && secondaryIsValidMonth) // /ranking/2019 or /ranking/2019/10
+  ) {
+    return true;
+  }
+  return false;
+}
 
 /**
  * Handle requests for `ranking` routes
@@ -16,30 +40,42 @@ function handleRankingRoute(server, renderCallback) {
     res.redirect(301, `/ranking/${defaultLocation}`);
   });
 
-  server.get(
-    `/ranking/:location(${locationsReStr})/:reqPrimary($|${rankingTypeReStr})/:reqSecondary($|${monthReStr})`,
-    (req, res) => {
-      res.send(JSON.stringify(req.params));
-      // renderCallback(req, res, '/ranking_v2', {
-      //   query: req.query,
-      //   params: req.params,
-      // });
-    },
-  );
-
+  // single, alltime, activities, etc
   server.get(
     `/ranking/:type(${allowedRankingTypes.join('|')})`,
-    ({ params: { type, filter = false } }, res) => {
-      // redirect to /ranking/{defaultLocation}/{rankingType}?/{rankingFilter}
-      res.redirect(301, `/ranking/${defaultLocation}/${type}${filter ? `/${filter}` : ''}`);
+    (req, res) => {
+      const { params: { type = '' } } = req;
+      res.redirect(301, `/ranking/${defaultLocation}/${type}`);
+    },
+  );
+
+  // year and optional month
+  server.get(
+    `/ranking/:year(${yearReStr})/:month?`,
+    (req, res) => {
+      const { params: { year, month = '' } } = req;
+      // Validate month because regex not working with optional param
+      if (month && !/(?:(?:0\d)|1[0-2])/.test(month)) {
+        // 404
+        return;
+      }
+      res.redirect(301, `/ranking/${defaultLocation}/${year}/${month}`);
     },
   );
 
   server.get(
-    `/ranking/:year(${yearReStr})/:month(${monthReStr})?`,
-    ({ params: { year, month = false } }, res) => {
-      // redirect to /ranking/{defaultLocation}/{rankingType}?/{rankingFilter}
-      res.redirect(301, `/ranking/${defaultLocation}/${year}${month ? `/${month}` : ''}`);
+    `/ranking/:location(${locationsReStr})/:reqPrimary?/:reqSecondary?`,
+    (req, res) => {
+      const { params } = req;
+      if (!requestParamsAreValid(params)) {
+        // 404
+        return;
+      }
+
+      renderCallback(req, res, '/ranking_v2', {
+        query: req.query,
+        params: req.params,
+      });
     },
   );
 }
