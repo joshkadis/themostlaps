216842f0971c5b07450c82fca595c5be386bd85f
Josh Kadis <josh@kadisco.com>
2020-03-04 22:09:22 -0500

    Handle invalid location label

--- components/pages/ranking/LocationHero.js
+++ components/pages/ranking/LocationHero.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { getLocation } from '../../../utils/v2/locations';
 
 const LocationHero = ({ location }) => (
-  <h2>{getLocation(location).locationLabel}</h2>
+  <h2>{getLocation(location).locationLabel || ''}</h2>
 );
 
 LocationHero.propTypes = {
