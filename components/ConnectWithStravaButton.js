9d28b90f92ecadfaa2a6efb2a38ac228f45cf859
Josh Kadis <josh@kadisco.com>
2020-05-17 08:27:24 -0400

    lint file

--- components/ConnectWithStravaButton.js
+++ components/ConnectWithStravaButton.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import * as styles from './ConnectWithStravaButton.css';
 import { trackConnectWithStrava } from '../utils/analytics';
